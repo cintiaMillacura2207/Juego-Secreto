d504e5ec3f7782af6a317a30c0f5f51af17b284f
cintiaMillacura22207 <37818974+cintiaMillacura2207@users.noreply.github.com>
2025-01-21 19:50:40 -0300

    Agregar funcion verificar intento

--- app.js
+++ app.js
@@ -8,3 +8,22 @@ function asignarTextoElemento(elemento,texto){
     elementoHTML.innerHTML = texto;
     return;
 }
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(intentos);
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1 ) ? 'vez':'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        // El usuario no acertó
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
