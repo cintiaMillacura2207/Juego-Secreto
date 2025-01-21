20d0da6f027d4e61dbbd20550398b7ad047fbd84
cintiaMillacura22207 <37818974+cintiaMillacura2207@users.noreply.github.com>
2025-01-21 19:49:47 -0300

    Agregar funcion asignar texto elemento

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ let numeroSecreto = 0;
 let intentos = 0;
 let listaNumerosSorteados = [];
 let numeroMaximo = 10;
+
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
